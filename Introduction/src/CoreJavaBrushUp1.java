817376c8870eeb7434ddad79bd8314c7c0fa979d
Mandy Mieu <mandymieu@gmail.com>
2024-04-10 22:07:34 -0600

    added variables

--- Introduction/src/CoreJavaBrushUp1.java
+++ Introduction/src/CoreJavaBrushUp1.java
@@ -4,7 +4,13 @@ public class CoreJavaBrushUp1 {
 	public static void main(String[] args) {
 		
 		int num = 5;
-
+		String website = "Rahul Shetty Academy";
+		char letter = 'r';
+		double dec = 5.99;
+		boolean myCard = true;
+		
+		System.out.println(num + " is the value stored in the num variable");
+		System.out.println(website);
 	}
 
 }
