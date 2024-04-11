69dc0e334d16678399013499bdf8c74a9ab52d58
Mandy Mieu <mandymieu@gmail.com>
2024-04-10 23:03:47 -0600

    added string modification

--- Introduction/src/CoreJavaBrushUp3.java
+++ Introduction/src/CoreJavaBrushUp3.java
@@ -1,9 +1,35 @@
+import org.openjdk.jol.vm.VM;
 
 public class CoreJavaBrushUp3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// string is an object
+		
+		// string literal
+//		String s = "Rahul Shetty Academy";
+//		String s1 = "Rahul Shetty Academy";
+//		System.out.println("The memory address is " + VM.current().addressOf(s));
+//		System.out.println("The memory address is " + VM.current().addressOf(s1));
+		// Because "Rahul Shetty Academy" already exists in memory, s1 uses the same address
+		String s5 = "hello"; // because hello doesn't already exist, a new Object is created
+		
+		// string with new keyword
+		String s2 = new String("Welcome");
+		String s3 = new String("Welcome");
+//		System.out.println("The memory address is " + VM.current().addressOf(s2));
+//		System.out.println("The memory address is " + VM.current().addressOf(s3));
+		// This will always create a new object if you use new
+		// In Java, string is an Object representing a sequence of characters
+		
+		String s = "Rahul Shetty Academy";
+		String[] splittedString = s.split("Shetty");
+		System.out.println(splittedString[0].trim());
+		System.out.println(splittedString[1].trim());
+		//System.out.println(splittedString[2]);
+		
+		for (int i = s.length() - 1; i >= 0; i--) {
+			System.out.println(s.charAt(i));
+		}
 	}
 
 }
