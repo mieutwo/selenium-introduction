5bcee8ca5776e61670d2b78002aaf84e3e080590
Mandy Mieu <mandymieu@gmail.com>
2024-04-10 23:08:57 -0600

    Added getData method

--- Introduction/src/MethodsDemo.java
+++ Introduction/src/MethodsDemo.java
@@ -2,8 +2,14 @@
 public class MethodsDemo {
 
 	public static void main(String[] args) {
-		
+		MethodsDemo d = new MethodsDemo();
+		String name = d.getData();
+		System.out.println(name);
 
 	}
 
+	public String getData() {
+		System.out.println("hello world");
+		return "rahul shetty";
+	}
 }
