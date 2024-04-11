21e1a8f6ba54fcb73f5c740226eda78aded2c9e0
Mandy Mieu <mandymieu@gmail.com>
2024-04-10 23:17:05 -0600

    Added implementation of MethodsDemo2 to MethodsDemo

--- Introduction/src/MethodsDemo.java
+++ Introduction/src/MethodsDemo.java
@@ -2,13 +2,17 @@
 public class MethodsDemo {
 
 	public static void main(String[] args) {
-		MethodsDemo d = new MethodsDemo();
-		String name = d.getData();
+//		MethodsDemo d = new MethodsDemo();
+//		String name = d.getData();
+		
+		String name = getData();
 		System.out.println(name);
-
+		MethodsDemo2 d1 = new MethodsDemo2();
+		d1.getUserData();
 	}
 
-	public String getData() {
+	// static gives class-level access to method
+	public static String getData() {
 		System.out.println("hello world");
 		return "rahul shetty";
 	}
--- Introduction/src/MethodsDemo2.java
+++ Introduction/src/MethodsDemo2.java
@@ -1,9 +1,8 @@
 
 public class MethodsDemo2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String getUserData() {
+		System.out.println("hello world");
+		return "rahul shetty";
 	}
-
 }
