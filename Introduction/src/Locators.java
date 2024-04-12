11318c5048f5f7dbb3e47e952778b24f73d24a8d
Mandy Mieu <mandymieu@gmail.com>
2024-04-12 15:28:59 -0600

    added selectors for email and password field in forgot your password

--- Introduction/src/Locators.java
+++ Introduction/src/Locators.java
@@ -23,6 +23,15 @@ public class Locators {
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+		
+		// XPath if no unique identifier: //input[@type='text'][2]
+		// CSS Selector: input[type='text']:nth-child(3)
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).clear(); // clears field
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@gmail.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("1234567890");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
 		//driver.close();
 	}
 
