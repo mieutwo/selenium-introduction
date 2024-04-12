d3fb35d916ccd5c5435c9e4ab34ad182659efe67
Mandy Mieu <mandymieu@gmail.com>
2024-04-11 18:29:29 -0600

    Added password entry and clicking sign in button

--- Introduction/src/Locators.java
+++ Introduction/src/Locators.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Locators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:/Browser Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
 	}
 
 }
