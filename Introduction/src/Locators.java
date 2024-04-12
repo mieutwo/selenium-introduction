52cbb0df71aa5cfeb32c4e2f8f46f82be8c44d38
Mandy Mieu <mandymieu@gmail.com>
2024-04-11 18:19:27 -0600

    Initialize Locators

---
+++ Introduction/src/Locators.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:/Browser Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
