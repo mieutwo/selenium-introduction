85b820b20b6ff021010a0601c062f6dbf9a8c392
Mandy Mieu <mandymieu@gmail.com>
2024-04-21 15:25:19 -0600

    Added getPassword function and switched from close() to quit()

--- Introduction/src/Locators2.java
+++ Introduction/src/Locators2.java
@@ -15,9 +15,11 @@ public class Locators2 {
 		// implicit wait - 2 seconds time out
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
+		String password = getPassword(driver);
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		
 		// implicit wait is not useful here because there are already a p element on the page before
@@ -25,14 +27,33 @@ public class Locators2 {
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
 		driver.findElement(By.xpath("//*[text()='Log Out']")).click(); // * is wild card
-		driver.close();
+		driver.quit(); // driver.close() causes socket connection reset error
+	}
+	
+	public static String getPassword(WebDriver driver) {
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		// Please use temporary password 'rahulshettyacademy' to Login.
+		// Let's split on '
+		String[] passwordArray = passwordText.split("'");
+		// [0] = "Please use temporary password "
+		// [1] = "rahulshettyacademy"
+		// [2] = " to Login."
+		String password = passwordArray[1];
+		return password;
 	}
 
 }
